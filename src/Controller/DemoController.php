2a13250f0a0ff4134225bc98fcc761c441824147
Ridha Rahmi <ridha.rahmi13@gmail.com>
2019-02-21 12:26:48 +0100

    update controller and add link menu

--- src/Controller/DemoController.php
+++ src/Controller/DemoController.php
@@ -3,14 +3,16 @@
 /**
  * custom module drupal 8
  */
+
 namespace Drupal\demo\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
 
-class DemoController extends ControllerBase {
+class DemoController extends ControllerBase
+{
 
-
-    public function content() {
+    public function content()
+    {
         $build = [
             '#type' => '#markup',
             '#markup' => t('Hello , this is project drupal 8'),
